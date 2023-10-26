607b948d4043dccc7991904e94066df4949f9456
chiwei.huang <chiwei.huang@chiweihuangdeMacBook-Air.local>
2023-10-26 23:00:36 +0800

    add storage layout and state variables

--- src/UsdcV2.sol
+++ src/UsdcV2.sol
@@ -4,25 +4,49 @@ pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract UsdcV2 is ERC20 {
+    // FiatTokenProxy storage layout
+    address	public _owner;
+    address public pauser;
+    bool public	paused;
+    address public blacklister;
+    mapping(address => bool) public blacklisted;
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    string public currency;
+    address	public masterMinter;
+    bool public initialized;
+    mapping(address => uint256) public balances;
+    mapping(address => mapping(address => uint256)) public allowed;
+    uint256 public totalSupply_;
+    mapping(address => bool) public minters;
+    mapping(address => uint256)	public minterAllowed;
+    address public _rescuer;
+    bytes32	DOMAIN_SEPARATOR;
+    mapping(address => mapping(bytes32 => bool)) public _authorizationStates;
+    mapping(address => uint256) public _permitNonces;
+    uint8 public _initializedVersion;
+
+    // UsdcV2 storage
     mapping(address => bool) public whitelist;
 
     constructor(string memory name, string memory symbol) ERC20(name, symbol){}
 
-    modifier isOwner {
-        require(msg.sender == 0xFcb19e6a322b27c06842A71e8c725399f049AE3a, "Not Owner");
+    modifier onlyOwner {
+        require(msg.sender == _owner, "Not Owner");
         _;
     }
 
-    modifier isWhitelist {
-            require(whitelist[msg.sender], "Not in Whitelist!");
-            _;
+    modifier onlyWhitelist {
+        require(whitelist[msg.sender], "Not in Whitelist!");
+        _;
     }
 
-    function addToWhitelist(address addr) external isOwner {
+    function addToWhitelist(address addr) external onlyOwner {
         whitelist[addr] = true;
     }
 
-    function transfer(address to, uint256 value) override public virtual isWhitelist returns (bool) {
+    function transfer(address to, uint256 value) override public virtual onlyWhitelist returns (bool) {
         address owner = _msgSender();
         _transfer(owner, to, value);
         return true;
